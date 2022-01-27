81bcbf30aa4e87da3138b1da553d5be4f7d19afc
Sajjad Fallahi <92434563+SJFMichael@users.noreply.github.com>
2022-01-27 11:55:11 +0330

    Add files via upload

---
+++ E-Health Management/src/ManagementSystem/Report.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ManagementSystem;
+
+import static ManagementSystem.Types.pIDs;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author UPC
+ */
+public class Report implements Serializable {
+	// equal to Patient ID - pID
+	private int reportNum;
+	private String status;
+	private ArrayList<Appointment> appList;
+
+	public Report(int reportNum) {
+		if ((!pIDs.contains(reportNum)) || reportNum < 0) {
+			System.out.println("There is no such a Patient - Non valid ID" + reportNum);
+			this.reportNum = -1;
+		} else {
+			this.reportNum = reportNum;
+			this.appList = new ArrayList<>();
+		}
+	}
+
+	public int getReportNum() {
+		return reportNum;
+	}
+
+	public void setReportNum(int reportNum) {
+		if (!pIDs.contains(reportNum) || reportNum < 0) {
+			System.out.println("There is no such a Patient - Non valid ID");
+			this.reportNum = -1;
+		} else {
+			this.reportNum = reportNum;
+		}
+	}
+	
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public void addToReport(LocalDate addDate, Doctor addDoc, Disease addDis, ArrayList<Drug> addDrugs, String description) {
+		Appointment apt = new Appointment(addDate, addDoc, addDis, addDrugs, description);
+		appList.add(apt);
+	}
+
+	public void addToReport(LocalDate addDate, Doctor addDoc, Disease addDis, ArrayList<Drug> addDrugs) {
+		Appointment apt = new Appointment(addDate, addDoc, addDis, addDrugs);
+		appList.add(apt);
+	}
+
+	public ArrayList<Appointment> getAppList() {
+		return appList;
+	}
+}
